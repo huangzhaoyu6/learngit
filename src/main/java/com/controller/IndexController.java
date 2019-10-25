214f4ec9ee8e45eba6525f6a7ba4514ca6ea9253
huangzhaoyu6 <15534465135@163.com>
2019-10-25 15:31:47 +0800

    mmmmmmmmmmmmmmmmm

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @RequestMapping(path={"/index","/"})
+    @ResponseBody
+    public String index(){
+        return "Hello World";
+    }
+}
