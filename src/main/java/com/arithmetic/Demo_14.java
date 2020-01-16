730188ba9d3618f50afc278b72fafdf73cd7dabb
huangzhaoyu6 <15534465135@163.com>
2020-01-16 16:33:40 +0800

    leetcode17

--- src/main/java/com/arithmetic/Demo_14.java
+++ src/main/java/com/arithmetic/Demo_14.java
@@ -19,7 +19,9 @@ public class Demo_14 {
         String prefix = strs[0];
         for (int i = 1; i < strs.length; i++) {
             //该方法为获取公共前缀  LCP(prefix，str)
+            //判断第i个字符串中是否包含公共前缀
             while (strs[i].indexOf(prefix) != 0) {
+                //如果不包含 去掉当前的公共前缀的最后一位
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()){ return "";}
             }
@@ -27,9 +29,42 @@ public class Demo_14 {
         return prefix;
     }
 
+    /**
+     *
+     *
+     *
+     */
+
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0 , strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        }
+        else {
+            int mid = (l + r)/2;
+            String lcpLeft =   longestCommonPrefix(strs, l , mid);
+            String lcpRight =  longestCommonPrefix(strs, mid + 1,r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    String commonPrefix(String left,String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if ( left.charAt(i) != right.charAt(i) ){
+                return left.substring(0, i);
+            }
+        }
+        return left.substring(0, min);
+    }
+
     public static void main(String[] args) {
         String[] str = { "qaq2qwe","qaqq3we","qaqqw4e"};
-        System.out.println(longestCommonPrefix(str));
+        System.out.println(new Demo_14().longestCommonPrefix2(str));
     }
 
 }
